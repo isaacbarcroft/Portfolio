35109614063bbca53c3629e27eb6604df2299a48
Isaac Barcroft <i_barcroft@yahoo.com>
2023-11-29 09:04:11 -0500

    loading phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,156 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./fonts/BroadwayD.ttf";
 import "./App.css";
+import headshot from "./images/headshot.jpg";
 import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Simulate the end of loading after a certain time (you can replace this with your actual loading logic)
+    }, 1500); // Change the duration as needed
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
   return (
     <div className="App">
       <div className="App-header">
-        <div
-          style={{
-            fontWeight: "bold",
-            fontSize: "3rem",
-            fontFamily: "BroadwayD",
-            color: "##F990E8",
-          }}
-        >
-          Isaac Barcroft
-        </div>
-        <div
-          style={{
-            color: "##F990E8",
-            fontStyle: "italic",
-            fontFamily: "BroadwayD",
-          }}
-        >
-          Software Engineer
-        </div>
-        <Footer />
-      </div>
-      <div className="App-main">
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a, in
-          atque cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! quidem
-          quia reiciendis nulla aliquid, minima doloremque ipsam reprehenderit
-          natus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-          a, in atque cupiditate debitis saepe impedit sunt laudantium neque
-          molestias quidem quia reiciendis nulla aliquid, minima doloremque
-          ipsam reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! quidem
-          quia reiciendis nulla aliquid, minima doloremque ipsam reprehenderit
-          natus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-          a, in atque cupiditate debitis saepe impedit sunt laudantium neque
-          molestias quidem quia reiciendis nulla aliquid, minima doloremque
-          ipsam reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! quidem
-          quia reiciendis nulla aliquid, minima doloremque ipsam reprehenderit
-          natus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-          a, in atque cupiditate debitis saepe impedit sunt laudantium neque
-          molestias quidem quia reiciendis nulla aliquid, minima doloremque
-          ipsam reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Odit a, in atque
-          cupiditate debitis saepe impedit sunt laudantium neque molestias
-          quidem quia reiciendis nulla aliquid, minima doloremque ipsam
-          reprehenderit natus! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odit a, in atque cupiditate debitis saepe impedit
-          sunt laudantium neque molestias quidem quia reiciendis nulla aliquid,
-          minima doloremque ipsam reprehenderit natus! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odit a, in atque cupiditate debitis
-          saepe impedit sunt laudantium neque molestias quidem quia reiciendis
-          nulla aliquid, minima doloremque ipsam reprehenderit natus!
-        </div>
+        {loading ? (
+          <p
+            className="shimmer-loader"
+            style={{
+              fontWeight: "bold",
+              fontSize: "1rem",
+              fontFamily: "BroadwayD",
+              color: "##F990E8",
+            }}
+          >
+            Isaac Barcroft's Portfolio
+          </p>
+        ) : (
+          <>
+            <img
+              src="src/assets/headshot.jpg"
+              className="App-logo"
+              alt="logo"
+              style={{ borderRadius: "50%" }}
+            />
+            <div
+              data-content="Isaac Barcroft"
+              className="shimmer-effect"
+              style={{
+                fontWeight: "bold",
+                fontSize: "3rem",
+                fontFamily: "BroadwayD",
+                color: "##F990E8",
+              }}
+            >
+              Isaac Barcroft
+            </div>
+            <div
+              className="shimmer-effect"
+              style={{
+                color: "##F990E8",
+                fontStyle: "italic",
+                fontFamily: "BroadwayD",
+              }}
+            >
+              Software Engineer
+            </div>
+            <Footer />
+          </>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
