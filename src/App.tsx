d43a6a015d3622e85e9f17e6390bd530a1ebf41a
Isaac Barcroft <i_barcroft@yahoo.com>
2023-11-30 10:25:22 -0500

    update format

--- src/App.tsx
+++ src/App.tsx
@@ -14,31 +14,33 @@ const App = () => {
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
   }, []);
 
+  if (loading)
+    return (
+      <div className="Loading">
+        <p
+          className="shimmer-loader"
+          style={{
+            fontWeight: "bold",
+            fontSize: "2rem",
+            fontFamily: "BroadwayD",
+            color: "#F990E8",
+          }}
+        >
+          Isaac Barcroft's Portfolio
+        </p>
+      </div>
+    );
   return (
     <div className="App">
       <div className="App-header">
-        {loading ? (
-          <p
-            className="shimmer-loader"
-            style={{
-              fontWeight: "bold",
-              fontSize: "1rem",
-              fontFamily: "BroadwayD",
-              color: "##F990E8",
-            }}
-          >
-            Isaac Barcroft's Portfolio
-          </p>
-        ) : (
-          <>
-            <img
-              src={require("/Users/isaacbarcroft/Documents/Portfolio/Portfolio/src/assets/headshot.jpg")}
-              className="App-logo"
-              alt="logo"
-              width="25%"
-              height="35%"
-              style={{ borderRadius: "50%" }}
-            />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
             <div
               data-content="Isaac Barcroft"
               className="shimmer-effect"
@@ -46,7 +48,7 @@ const App = () => {
                 fontWeight: "bold",
                 fontSize: "3rem",
                 fontFamily: "BroadwayD",
-                color: "##F990E8",
+                color: "#F990E8",
               }}
             >
               Isaac Barcroft
@@ -54,16 +56,30 @@ const App = () => {
             <div
               className="shimmer-effect"
               style={{
-                color: "##F990E8",
+                color: "#F990E8",
                 fontStyle: "italic",
                 fontFamily: "BroadwayD",
               }}
             >
               Software Engineer
             </div>
-            <Footer />
-          </>
-        )}
+          </div>
+          <img
+            style={{
+              display: "flex",
+              border: "1px #0bd2d3 solid",
+              borderRadius: "50%",
+              margin: 10,
+              boxShadow: "0 0 10px #0bd2d3",
+            }}
+            src={require("/Users/isaacbarcroft/Documents/Portfolio/Portfolio/src/assets/profile.jpg")}
+            className="App-logo"
+            alt="head shot"
+            width="150px"
+            height="150px"
+          />
+          <Footer />
+        </div>
       </div>
     </div>
   );
