dfa21b5cdd22f62d974693e0553bdcf9125288b5
Isaac Barcroft <i_barcroft@yahoo.com>
2023-12-01 12:43:02 -0500

    greeting component

---
+++ src/components/Greeting/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import "../../fonts/BroadwayD.ttf";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 40%;
+  font-weight: 600;
+  color: #f990e8;
+  margin: 0 16px;
+`;
+
+export const Title = styled.div`
+  font-weight: 600;
+  font-family: BroadwayD;
+  font-size: 2rem;
+  margin-bottom: 8px;
+`;
+
+export const Text = styled.div`
+  color: #0bd2d3;
+  margin-bottom: 16px;
+`;
