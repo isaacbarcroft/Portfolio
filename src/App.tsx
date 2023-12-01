305d080680c6c035c64d50798223a1cc7c3f22fc
Isaac Barcroft <i_barcroft@yahoo.com>
2023-12-01 12:43:19 -0500

    main app build out

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./fonts/BroadwayD.ttf";
 import "./App.css";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Greeting from "./components/Greeting";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [showMainApp, setShowMainApp] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false); // Simulate the end of loading after a certain time (you can replace this with your actual loading logic)
-    }, 1500); // Change the duration as needed
+      setLoading(false);
+      setShowMainApp(true);
+    }, 1500);
 
-    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    return () => clearTimeout(timer);
   }, []);
 
   if (loading)
@@ -33,53 +37,32 @@ const App = () => {
   return (
     <div className="App">
       <div className="App-header">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
-          }}
-        >
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div
-              data-content="Isaac Barcroft"
-              className="shimmer-effect"
-              style={{
-                fontWeight: "bold",
-                fontSize: "3rem",
-                fontFamily: "BroadwayD",
-                color: "#F990E8",
-              }}
-            >
-              Isaac Barcroft
-            </div>
-            <div
-              className="shimmer-effect"
-              style={{
-                color: "#F990E8",
-                fontStyle: "italic",
-                fontFamily: "BroadwayD",
-              }}
-            >
-              Software Engineer
-            </div>
-          </div>
-          <img
+        <Header setShowMainApp={setShowMainApp} />
+        <div className="content-Container ">
+          {showMainApp && <Greeting />}
+          <div
             style={{
               display: "flex",
-              border: "1px #0bd2d3 solid",
-              borderRadius: "50%",
-              margin: 10,
-              boxShadow: "0 0 10px #0bd2d3",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              width: "40%",
             }}
-            src={require("/Users/isaacbarcroft/Documents/Portfolio/Portfolio/src/assets/profile.jpg")}
-            className="App-logo"
-            alt="head shot"
-            width="150px"
-            height="150px"
-          />
-          <Footer />
+          >
+            <div
+              style={{
+                fontFamily: "BroadwayD",
+                fontWeight: "bold",
+                fontSize: "1rem",
+                marginBottom: 8,
+              }}
+              onClick={() => setShowMainApp(false)}
+            >
+              My Work
+            </div>
+          </div>
         </div>
+
+        <Footer />
       </div>
     </div>
   );
