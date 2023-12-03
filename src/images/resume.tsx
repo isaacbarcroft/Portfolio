f126430fdc4dae0cd6613cfd9c714a5452441eb5
Isaac Barcroft <i_barcroft@yahoo.com>
2023-12-02 19:57:40 -0500

    link resume

--- src/images/resume.tsx
+++ src/images/resume.tsx
@@ -1,13 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ResumeIcon: React.FC = () => {
   return (
-    <div className="anchor-link">
-      <a
-        href="https://www.linkedin.com/in/isaac-barcroft/"
-        target="_blank"
-        rel="noreferrer"
-      >
+    <Link to="/resume" target="_blank" className="link">
+      <div className="anchor-link">
         <svg
           data-name="Layer 1"
           id="Layer_1"
@@ -44,8 +41,8 @@ const ResumeIcon: React.FC = () => {
             d="M10,16a1,1,0,0,0,1-1,2.11,2.11,0,0,1,.86-.64,2,2,0,0,0,.84-.41,1.63,1.63,0,0,0,.31,0,1.73,1.73,0,0,0,.32,0,2,2,0,0,0,.85.41,2.24,2.24,0,0,1,.83.62A1,1,0,0,0,17,15c0-1.24-1.3-2.05-1.93-2.34a4.24,4.24,0,0,0,.43-2.16A2.6,2.6,0,0,0,13,8a2.6,2.6,0,0,0-2.5,2.5,4.24,4.24,0,0,0,.43,2.16C10.3,13,9,13.76,9,15A1,1,0,0,0,10,16Zm3-6a.64.64,0,0,1,.5.5c0,.75,0,1.1-.46,1.54H13c-.44-.44-.46-.79-.46-1.54A.64.64,0,0,1,13,10Z"
           />
         </svg>
-      </a>
-    </div>
+      </div>
+    </Link>
   );
 };
 
